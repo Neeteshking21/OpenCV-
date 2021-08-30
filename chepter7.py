65c71016d2d9e46aa2018f88d65e65a337d9b995
NeeteshKing21 <neeteshking21@gmail.com>
2021-08-30 22:03:19 +0530

    chepter 7 -> detect colors

---
+++ chepter7.py
@@ -0,0 +1,44 @@
+# color detection
+
+import cv2
+import numpy as np
+
+def empty(a):
+    pass
+
+
+cv2.namedWindow('TrackBars')
+cv2.resizeWindow('TrackBars', 640,240)
+cv2.createTrackbar('Hue min', 'TrackBars', 0, 179, empty)
+cv2.createTrackbar('Hue max', 'TrackBars', 179, 179, empty)
+cv2.createTrackbar('Sat min', 'TrackBars', 0, 255, empty)
+cv2.createTrackbar('Sat max', 'TrackBars', 255, 255, empty)
+cv2.createTrackbar('Val min', 'TrackBars', 0, 255, empty)
+cv2.createTrackbar('Val max', 'TrackBars', 255, 255, empty)
+
+while True:
+
+    img = cv2.imread('src/dp.jpg')
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    h_min = cv2.getTrackbarPos('Hue min', 'TrackBars')
+    h_max = cv2.getTrackbarPos('Hue max', 'TrackBars')
+    s_min = cv2.getTrackbarPos('Sat min', 'TrackBars')
+    s_max = cv2.getTrackbarPos('Sat max', 'TrackBars')
+    v_min = cv2.getTrackbarPos('Val min', 'TrackBars')
+    v_max = cv2.getTrackbarPos('Val max', 'TrackBars')
+
+    lower = np.array([h_min,s_min,v_min])
+    upper = np.array([h_max,s_max,v_max])
+
+    mask = cv2.inRange(imgHSV,lower,upper)
+    result = cv2.bitwise_or(img, img, mask=mask)
+    print(h_min,h_max,s_min,s_max,v_min,v_max)
+
+    cv2.imshow('Color detection', img)
+    cv2.imshow('HSV', imgHSV)
+    cv2.imshow('Mask', mask)
+    cv2.imshow('Result', result)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        print("Stop")
+        break
